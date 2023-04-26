36eb7e6d9d5d4d10d2ce5325d25efbae6236d787
Yuri Kruhovyi <yurii.kruhovii@gosolo.net>
2023-04-26 22:50:17 +0300

    rename

--- client/src/modules/houses/components/index.js
+++ client/src/modules/houses/components/index.js
@@ -1,17 +1,15 @@
-import {useDispatch, useSelector} from "react-redux";
-import {useEffect} from "react";
-import {blancAction, startUp} from "../actions";
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { blancAction, startUp } from '../actions';
 
 export default () => {
-	const state = useSelector((state) => state)
-	const dispatch = useDispatch()
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		console.log('blac')
-		// dispatch(startUp())
-	}, []);
+  useEffect(() => {
+    console.log('blac');
+    // dispatch(startUp())
+  }, []);
 
-	return (
-		<div>Houses</div>
-	)
-}
+  return <div>Houses</div>;
+};
