841a2ecd4baceef790aa8a4380fd5680458d2f17
Yuri Kruhovyi <yurii.kruhovii@gosolo.net>
2023-04-29 11:53:50 +0300

    old config

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -40,43 +40,8 @@ module.exports = {
       'error',
       { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
     ],
-    'unused-imports/no-unused-imports': 'error',
+    // '@typescript-eslint/no-unused-vars': 'off',
+    // 'no-unused-vars': 'warn',
+    // 'unused-imports/no-unused-imports': 'error',
   },
 };
-// "eslintConfig": {
-//   "extends": [
-//     "airbnb-typescript",
-//     "airbnb/hooks",
-//     "plugin:@typescript-eslint/recommended",
-//     "plugin:jest/recommended",
-//     "plugin:prettier/recommended"
-//   ],
-//     "plugins": ["react", "@typescript-eslint", "jest"],
-//     "env": {
-//     "browser": true,
-//       "es6": true,
-//       "jest": true
-//   },
-//   "globals": {
-//     "Atomics": "readonly",
-//       "SharedArrayBuffer": "readonly"
-//   },
-//   "parser": "@typescript-eslint/parser",
-//     "parserOptions": {
-//     "ecmaFeatures": {
-//       "jsx": true
-//     },
-//     "ecmaVersion": 2018,
-//       "sourceType": "module",
-//       "project": "./tsconfig.json"
-//   },
-//   "rules": {
-//     "linebreak-style": "off",
-//       "prettier/prettier": [
-//       "error",
-//       {
-//         "endOfLine": "auto"
-//       }
-//     ]
-//   }
-// },
